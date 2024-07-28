4d4fd182ed0d1a3de8ab6ea9129d73816697c38d
JJJJJllll <jishilong200005@gmail.com>
2024-07-28 23:30:58 +0800

    改angle mode debug测试pidLevel

---
+++ src/main/target/STM32F405/target.mk
@@ -0,0 +1,17 @@
+TARGET_MCU        := STM32F405xx
+TARGET_MCU_FAMILY := STM32F4
+
+# forked from STM32F445 target.mk 240728 by JJJJJllll
+TARGET_SRC = \
+            drivers/accgyro/accgyro_mpu6500.c \
+            drivers/accgyro/accgyro_spi_mpu6500.c \
+            drivers/accgyro/accgyro_spi_mpu9250.c \
+            drivers/accgyro/accgyro_virtual.c \
+            drivers/barometer/barometer_virtual.c \
+            drivers/compass/compass_virtual.c \
+            drivers/rx/rx_nrf24l01.c \
+            rx/nrf24_cx10.c \
+            rx/nrf24_inav.c \
+            rx/nrf24_h8_3d.c \
+            rx/nrf24_syma.c \
+            rx/nrf24_v202.c
